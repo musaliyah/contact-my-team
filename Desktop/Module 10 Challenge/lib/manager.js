280e05f4ea02a562868f19fa0c2895310e8ecd5f
musaliyah <asmusaliar@gmail.com>
2022-08-10 16:51:39 -0700

    lib employee, engineer, intern, manager files completed

---
+++ Desktop/Module 10 Challenge/lib/manager.js
@@ -0,0 +1,30 @@
+class Manager {
+    constructor(name, id, email, number){
+        this.name = name;
+        this.id = id;
+        this.email = email; 
+        this.number = number;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getNumber(){
+        return this.number;
+    }
+
+    getRole(){
+        return "Manager"
+    }
+
+}
+module.exports = Manager;
